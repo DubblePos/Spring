7a303692dd706c36758c1163fa0e843670bece81
wks1945 <wks1945@naver.com git config --global user.name Your>
2021-10-14 17:51:58 +0900

    2021/10/14 user,member 완성

--- Ch08/src/main/java/kr/co/ch08/dao/MemberDao.java
+++ Ch08/src/main/java/kr/co/ch08/dao/MemberDao.java
@@ -1,5 +1,16 @@
 package kr.co.ch08.dao;
 
-public class MemberDao {
+import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
+import kr.co.ch08.vo.MemberVo;
+
+@Repository
+public interface MemberDao {
+	public void insertMember(MemberVo vo);
+	public MemberVo selectMember(String uid);
+	public List<MemberVo> selectMembers();
+	public void updateMember(MemberVo vo);
+	public void deleteMember(String uid);
 }
