c2257788117856908efd73cbc6f7d0f9079edb10
wks1945 <wks1945@naver.com git config --global user.name Your>
2021-11-01 11:58:21 +0900

    2021/11/01 header, footer, aside 완료

--- Kmarket/src/main/java/kr/co/kmarket/KmarketApplication.java
+++ Kmarket/src/main/java/kr/co/kmarket/KmarketApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@MapperScan("kr.co.kmarket.dao")
+@MapperScan(value={"kr.co.kmarket.dao", "kr.co.kmarket.admin.dao"})
 @SpringBootApplication
 public class KmarketApplication {
 
