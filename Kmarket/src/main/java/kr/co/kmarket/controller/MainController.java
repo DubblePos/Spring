49c3bade7943f9caf1ebaddb2409b632c574a8d1
wks1945 <wks1945@naver.com git config --global user.name Your>
2021-11-05 08:57:47 +0900

    2021/11/05

--- Kmarket/src/main/java/kr/co/kmarket/controller/MainController.java
+++ Kmarket/src/main/java/kr/co/kmarket/controller/MainController.java
@@ -1,13 +1,42 @@
 package kr.co.kmarket.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.co.kmarket.service.MainService;
+import kr.co.kmarket.vo.CategoriesVo;
 
 @Controller
 public class MainController {
 	
-	@GetMapping(value = {"/","/index"})
-	public String index() {
+	@Autowired
+	private MainService service;
+	
+	@GetMapping(value = {"/", "/index"})
+	public String index(Model model) {
+		
+		//List<CategoriesVo> cates = service.selectCategories();
+		
+		//model.addAttribute("cates", cates);
+		
 		return "/index";
 	}
+	
+	@ResponseBody
+	@GetMapping("/getCategories")
+	public List<CategoriesVo> getCategories() {
+		
+		List<CategoriesVo> cates = service.selectCategories();
+		
+		return cates;
+		
+	}
+	
+	
+
 }
