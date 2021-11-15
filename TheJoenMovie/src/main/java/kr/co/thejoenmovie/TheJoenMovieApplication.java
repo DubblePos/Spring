ddf3aece887c7cb9800f2042a3ccab2de8b7998b
wks1945 <wks1945@naver.com git config --global user.name Your>
2021-11-15 11:14:48 +0900

    팀프로젝트 더조은무비

---
+++ TheJoenMovie/src/main/java/kr/co/thejoenmovie/TheJoenMovieApplication.java
@@ -0,0 +1,13 @@
+package kr.co.thejoenmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheJoenMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheJoenMovieApplication.class, args);
+	}
+
+}
